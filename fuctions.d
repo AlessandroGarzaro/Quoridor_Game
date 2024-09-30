e7b70d096d73f4e9eef84e5f0a5e832c2ae65aa9
Alessandro Garzaro <119001521+AlessandroGarzaro@users.noreply.github.com>
2024-09-30 16:47:50 +0200

    Add files via upload

---
+++ fuctions.d
@@ -0,0 +1,14 @@
+.\fuctions.o: RIT\Fuctions.c
+.\fuctions.o: C:\Users\alega\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.7.1\Device\Include\lpc17xx.h
+.\fuctions.o: C:\Users\alega\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm3.h
+.\fuctions.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\fuctions.o: C:\Users\alega\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h
+.\fuctions.o: C:\Users\alega\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h
+.\fuctions.o: C:\Users\alega\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armcc.h
+.\fuctions.o: C:\Users\alega\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\mpu_armv7.h
+.\fuctions.o: C:\Users\alega\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.7.1\Device\Include\system_LPC17xx.h
+.\fuctions.o: RIT\RIT.h
+.\fuctions.o: RIT\../Touchpanel/Touchpanel.h
+.\fuctions.o: RIT\../button_EXINT/button.h
+.\fuctions.o: RIT\../GLCD/GLCD.h
+.\fuctions.o: RIT\../timer/timer.h
